3c2e550348a92da1c0d4d9f7bc28ccc35105d3a0
Steven Flintham <sgf@lemma.co.uk>
2016-02-23 23:47:53 +0000

    Add TODO re pending optimisation post to examine

--- src/adfs150.asm
+++ src/adfs150.asm
@@ -911,6 +911,7 @@ ENDIF
        EQUS "Disc protected"
        EQUB &00
 ;;
+;; TODO: Look at dp11's optimisation suggestion (post Tue Feb 23, 2016 11:32 pm)
 IF NOT(PATCH_SD)        ;; Called only from Floppy and IDE code, not SD code
 .L831E JSR L8324	;; Wait until nor busy, then write command to command register
        BNE L82BD        ;; Generate disk error
